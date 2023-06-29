c1b19db54d048e39b94be1e6079acea97bdd6a83
RajThapa7 <jyangoraj123@gmail.com>
2023-06-29 12:00:29 +0545

    merge issue solved

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,10 +6,12 @@ import {
   Typography,
 } from "@material-tailwind/react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import useLogin from "../api/hooks/useLogin";
 
 export default function Login() {
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -22,7 +24,7 @@ export default function Login() {
     loginMutation.mutate(formData, {
       onSuccess: (data) => {
         console.log(data, "success");
-        window.location.href = "/todos";
+        navigate("/todos");
       },
       onError: (error) => {
         console.log(error, "error");
